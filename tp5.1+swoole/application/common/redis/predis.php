d0b4f6864c227f4e6ad43038a7b0880fb04134d3
Mac <>
2020-02-15 22:51:37 +0800

    添加phpredis 操作方法

---
+++ tp5.1+swoole/application/common/redis/predis.php
@@ -0,0 +1,69 @@
+<?php
+namespace app\common\redis;
+
+/**
+ * php 操作Redis
+ */
+class predis{
+
+    public $redis = "";
+
+    /**
+     * 定义单例模式变量
+     */
+    private static $_instance = null;
+
+    public static function getInstance(){
+        if(empty(self::$_instance)){    
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+
+    private function __construct(){
+        $this->redis = new \Redis(); //redis.so在全局路径下
+        $result = $this->redis->connect(config('redis.host'),config('redis.port'),config('redis.timeOut'));
+        if($result == false){
+            throw new \Exception('redis connect error');
+        }
+    }   
+
+    /**
+     * 设置
+     */
+    public function set($key, $value, $time = 0 ){
+        
+        if(!$key){
+            return '';
+        }
+
+        if(is_array($value)){
+            $value = json_encode($value);
+        }
+
+        if(!$time){
+            return $this->redis->set($key,$value);
+        }
+
+        return $this->redis->setex($key, $time, $value);
+
+    }
+
+    /**
+     * 获取
+     */
+
+    public function get($key, $value, $time = 0 ){
+        
+        if(!$key){
+            return '';
+        }
+
+        return $this->redis->get($key);
+
+    }
+
+
+}
---
+++ tp5.1+swoole/application/index/controller/Login.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+use app\common\aliyun\Sms;
+use app\common\Util;
+class Login
+{
+    public function login()
+    {
+
+        
+
+    }
+
+}
